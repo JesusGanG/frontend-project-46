d8326897acd914804df9711f2b1551506cfcdaf2
JesusGanG <vip.almosov@mail.ru>
2023-07-30 00:32:27 +0300

    add asciinema, mail and fix gendiff file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/JesusGanG/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/JesusGanG/frontend-project-46/actions)
+
+## Demonstration of the **genDiff** utility:
+### Compare files in stylish format:
+[![asciicast](https://asciinema.org/a/wTOyVJbDT1uVqqCvObO5qLjyG.svg)](https://asciinema.org/a/y1eDsnmfDcJlboyLhETJcKcw3)
--- gendiff.js
+++ gendiff.js
@@ -1,18 +1,23 @@
 #!/usr/bin/env node
 
+import { Command } from 'commander';
 import fs from 'fs';
 import path from 'path';
-import { Command } from 'commander';
 import _ from 'lodash';
 
+const program = new Command();
+
 const genDiff = (filepath1, filepath2) => {
-	const data1 = fs.readFileSync(filepath1, 'utf-8');
-	const data2 = fs.readFileSync(filepath2, 'utf-8');
+	const resolvedPath1 = path.resolve(filepath1);
+	const resolvedPath2 = path.resolve(filepath2);
+
+	const data1 = fs.readFileSync(resolvedPath1, 'utf-8');
+	const data2 = fs.readFileSync(resolvedPath2, 'utf-8');
 
 	const obj1 = JSON.parse(data1);
 	const obj2 = JSON.parse(data2);
 
-	const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
+	const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
 
 	const diff = keys.map((key) => {
 		if (_.has(obj1, key) && _.has(obj2, key)) {
@@ -30,14 +35,12 @@ const genDiff = (filepath1, filepath2) => {
 	return `{\n${diff.flat().join('\n')}\n}`;
 };
 
-const program = new Command();
 program.version('0.1.0');
 program.arguments('<filepath1> <filepath2>');
 program.description('Compares two configuration files and shows a difference.');
 program.action((filepath1, filepath2) => {
-	const resolvedPath1 = path.resolve(filepath1);
-	const resolvedPath2 = path.resolve(filepath2);
-	console.log(genDiff(resolvedPath1, resolvedPath2));
+	const diffOutput = genDiff(filepath1, filepath2);
+	console.log(diffOutput);
 });
 
 program.parse(process.argv);
