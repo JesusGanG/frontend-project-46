36d1d8ebe31f6ebd978c581bb2aa2340082b16e3
JesusGanG <vip.almosov@mail.ru>
2023-07-31 03:59:08 +0300

    add tests for parsers

---
+++ __tests__/parsers.test.js
@@ -0,0 +1,38 @@
+import stringParserToObject from '../src/parsers.js';
+
+const file1STR = `{
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+}`;
+
+const file1 = {
+	host: 'hexlet.io',
+	timeout: 50,
+	proxy: '123.234.53.22',
+	follow: false,
+};
+
+const file2YmlStr = `timeout: 20
+verbose: true
+host: "hexlet.io"`;
+
+const file2Yml = {
+	timeout: 20,
+	verbose: true,
+	host: 'hexlet.io',
+};
+
+describe('String Parser to object', () => {
+	test('JSON parser', () => {
+		expect(stringParserToObject(file1STR, 'JSON')).toEqual(file1);
+	});
+	test('YML parser', () => {
+		expect(stringParserToObject(file2YmlStr, 'YML')).toEqual(file2Yml);
+	});
+	// YML парсер нормально парсит JSON, потому ошибка только на JSON парсер
+	test('wrong format parser', () => {
+		expect(() => stringParserToObject(file2YmlStr, 'JSON')).toThrow();
+	});
+});
