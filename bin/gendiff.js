963e8e116995899f1477b82677b544d698a3712d
JesusGanG <vip.almosov@mail.ru>
2023-07-31 03:50:27 +0300

    fix code in gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,46 +1,20 @@
 #!/usr/bin/env node
-
-import fs from 'fs';
-import path from 'path';
 import { Command } from 'commander';
-import _ from 'lodash';
+import genDiff from '../src/index';
 
 const program = new Command();
 
-const genDiff = (filepath1, filepath2) => {
-	const resolvedPath1 = path.resolve(filepath1);
-	const resolvedPath2 = path.resolve(filepath2);
-
-	const data1 = fs.readFileSync(resolvedPath1, 'utf-8');
-	const data2 = fs.readFileSync(resolvedPath2, 'utf-8');
-
-	const obj1 = JSON.parse(data1);
-	const obj2 = JSON.parse(data2);
-
-	const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-
-	const diff = keys.map((key) => {
-		if (_.has(obj1, key) && _.has(obj2, key)) {
-			if (_.isEqual(obj1[key], obj2[key])) {
-				return `  ${key}: ${obj1[key]}`;
-			}
-			return [`+ ${key}: ${obj2[key]}`, `- ${key}: ${obj1[key]}`];
-		}
-		if (_.has(obj1, key)) {
-			return `- ${key}: ${obj1[key]}`;
-		}
-		return `+ ${key}: ${obj2[key]}`;
+program
+	.name('gendiff')
+	.description('Compares two configuration files and shows a difference.')
+	.version('1.0')
+	.option('-f, --format <type>', 'output format', 'stylish')
+	.argument('<filepath1>')
+	.argument('<filepath2>')
+	.action((filepath1, filepath2) => {
+		const options = program.opts();
+		// eslint-disable-next-line no-console
+		console.log(genDiff(filepath1, filepath2, options.format));
 	});
 
-	return `{\n${diff.flat().join('\n')}\n}`;
-};
-
-program.version('0.1.0');
-program.arguments('<filepath1> <filepath2>');
-program.description('Compares two configuration files and shows a difference.');
-program.action((filepath1, filepath2) => {
-	const diffOutput = genDiff(filepath1, filepath2);
-	console.log(diffOutput);
-});
-
-program.parse(process.argv);
+program.parse();
