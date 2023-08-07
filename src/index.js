44ff5b33b6df8dde0ff0f438e0f8e1ccdd1d9a04
JesusGanG <vip.almosov@mail.ru>
2023-08-07 16:41:48 +0300

    fix test problem

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ export const compareTreeFormer = (object1, object2) => {
 	return innerTreeFormer(object1, object2, 1);
 };
 
-export const genDiff = (path1, path2, format = 'stylish') => {
+const genDiff = (path1, path2, format = 'stylish') => {
 	if (path1.length === 0 || path2.length === 0) {
 		return 'enter valid path';
 	}
@@ -95,3 +95,5 @@ export const genDiff = (path1, path2, format = 'stylish') => {
 	const compareTree = compareTreeFormer(object1, object2);
 	return formatSelector(compareTree, format);
 };
+
+export default genDiff;
